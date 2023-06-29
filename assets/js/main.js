d6c082ab1a7d6f90ba97dec0cd0968827527af35
Kevin Kouakou <kevinkouakou113@gmail.com>
2023-06-29 16:40:05 +0000

    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,29 @@ navbar_items.forEach((navbar_item) => {
   navbar_item.addEventListener("click", () => {
     navbar_item.classList.add("isActive");
     navbar_items.forEach(function (otherItems) {
-        if (otherItems !== navbar_item) {
-          otherItems.classList.remove("isActive");
-        }
-      });
+      if (otherItems !== navbar_item) {
+        otherItems.classList.remove("isActive");
+      }
+    });
   });
-  
+});
+
+const swiper = new Swiper('.swiper', {
+  initialSlide: 2,
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 110,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    clickable: true,
+  },
 });
